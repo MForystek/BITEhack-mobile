1590ca7a488ad80f02820df3dc24157765be6e55
sszymicz <szymonszymiczek32@gmail.com>
2023-01-14 23:59:19 +0100

    messages mocking debug

--- app/src/main/java/com/dfs/dfsbitehackmobile/activity/MockService.java
+++ app/src/main/java/com/dfs/dfsbitehackmobile/activity/MockService.java
@@ -3,20 +3,25 @@ package com.dfs.dfsbitehackmobile.activity;
 import com.dfs.dfsbitehackmobile.dto.User;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MockService implements ServiceAPI {
 
     private static MockService mockServiceInstance = null;
-    private final User currentUser = new User("Adam", "adampączkizjadam2137@poczta.com", "green", Arrays.asList("purring", "sleeping"), Arrays.asList("killing", "meowing"));
-    private final User otherUser = new User("Mathew", "maciekpaciek@poczta.com", "red", Arrays.asList("guitar playing", "chess"), Arrays.asList("scuba diving", "driving"));
-    private List<Message> messages = Arrays.asList(
-            new Message("hej", currentUser, otherUser, Instant.parse("2023-01-14T18:35:24.00Z"), true),
-            new Message("cześć", otherUser, currentUser, Instant.parse("2023-01-14T18:38:24.00Z"),  false)
-    );
+    private final User currentUser;
+    private final User otherUser;
+    private final List<Message> messages;
     private MockService() {
         //normally connection here
+        currentUser = new User("Adam", "adampączkizjadam2137@poczta.com", "green", Arrays.asList("purring", "sleeping"), Arrays.asList("killing", "meowing"));
+        otherUser = new User("Mathew", "maciekpaciek@poczta.com", "red", Arrays.asList("guitar playing", "chess"), Arrays.asList("scuba diving", "driving"));
+        messages = new ArrayList<>();
+        messages.add(
+                new Message("cześć", otherUser, currentUser, Instant.parse("2023-01-14T18:38:24.00Z"),  false));
+        messages.add(
+                new Message("hej", currentUser, otherUser, Instant.parse("2023-01-14T18:35:24.00Z"), true));
     }
 
     public static MockService getInstance() {
