f45e12fc99e16d8500500f9a958afe8eba487d8a
xsalonx <lukasz.dubiel3@gmail.com>
2023-01-15 11:51:26 +0100

    signup refactor/ signin done

--- app/src/main/java/com/dfs/dfsbitehackmobile/activity/WelcomeActivity.java
+++ app/src/main/java/com/dfs/dfsbitehackmobile/activity/WelcomeActivity.java
@@ -20,7 +20,6 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         initializeComponents();
-        ServerConnector.getINSTANCE().signup();
     }
 
 
